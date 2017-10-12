f9ebf0fc9b61f6aeb787a9b7a0245933988fcb73
Lance Mustard <lance.mustard@hotmail.com>
2017-10-12 10:36:52 +0800

    leason 110

--- server/server.js
+++ server/server.js
@@ -16,14 +16,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'admin@example.com',
+    text: 'Welcome to the chat room',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'admin@example.com',
+    text: 'Another user has joined the chat room',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
 
+    // send to all sockets
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // sends to all sockets, except the socket that initialised the event
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   })
 
   socket.on('disconnect', () => {
