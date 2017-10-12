e0d5044ec34c65aa85db4093e226e87884ccec3b
Lance Mustard <Lance.Mustard@hotmail.com>
2017-10-12 20:11:15 +0800

    leason 115

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message.js');
+var {generateMessage, generateLocationMessage} = require('./message.js');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -7,6 +7,18 @@ describe('generateMessage', () => {
     var text = 'this is a text';
     var message = generateMessage(from, text);
     expect(message).toMatchObject({from, text});
+    expect(typeof(message.createdAt)).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'test@example.com';
+    var latitude = 32.111;
+    var longitude = 115.111;
+    var url = 'https://www.google.com/maps?q=32.111,115.111';
+    var message = generateLocationMessage(from, latitude, longitude);
+    expect(message).toMatchObject({from, url});
     expect(typeof(message.createdAt)).toBe('number');
   });
 });
